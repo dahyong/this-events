99296bc34342b2223e912d680d996825c0b2d6ca
Darrell Ah Yong <d_a_y_1@hotmail.com>
2017-10-25 11:50:57 -1000

    secret menu assignment

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var menuElem = document.getElementsByClassName("name");
 
+for(var i = 0; i<menuElem; i++){
+	menuElem[i].addeEventListener("click", showSecret);
+}
+
+function showSecret(){
+	var secretMenu = this.querySelectorAll(".menu")[0];
+	if(secretMenu.style.visability === "hidden"){
+		secretMenu.style.visability = "visible";
+	}else{
+		secretMenu.style.visability = "hidden";
+	}
+}
